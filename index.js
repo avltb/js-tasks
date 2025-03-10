5fdc6a351613b8c9de7cc76d61a57bbe5a663bd9
avltb <avtlboo@gmail.com>
2025-03-10 22:46:13 +0500

    add files

---
+++ index.js
@@ -0,0 +1,13 @@
+const acceptName = function(name){
+    if (name === "Жасмин") {
+        console.log("Сегодня твой счастливый день! Получи свой приз!");
+    } else if (name === "Айдана") {
+        console.log("Сегодня и твой счастливый день! Получи свою скидку!");
+    } else {
+        console.log("Добро пожаловать, "+name+"!");
+    }
+  }
+  acceptName("Кира");
+  acceptName("Жасмин");
+  acceptName("Айдана");
+  acceptName("Ксения");
